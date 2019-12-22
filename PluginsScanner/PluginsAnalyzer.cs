a0e6cc8df07926e8b784164de0f97f5a8b1e3a8d
ogendogen <kosmos500@interia.pl>
2019-12-22 20:18:59 +0100

    Implemented AnalyzePlugins

--- PluginsScanner/PluginsAnalyzer.cs
+++ PluginsScanner/PluginsAnalyzer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace PluginsScanner
 {
@@ -9,9 +10,18 @@ namespace PluginsScanner
         public List<string> LookupStrings { get; set; }
         public string FilesType { get; set; }
 
-        public void AnalyzePlugins(List<string> pluginsContent)
+        public IEnumerable<InfectionOccurence> AnalyzePlugins(List<Plugin> plugins)
         {
-
+            foreach (var plugin in plugins)
+            {
+                string[] lines = plugin.PluginDump.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                string[] foundLines = lines.Where(line => LookupStrings.Any(lookupString => line.Contains(lookupString))).ToArray();
+                
+                foreach (var foundLine in foundLines)
+                {
+                    yield return new InfectionOccurence{ FoundString = foundLine, LineNumber = Array.IndexOf(lines, foundLine + 1)};
+                }
+            }
         }
     }
 }
