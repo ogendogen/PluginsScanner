a75a92fbb67313d16421cf036ccb4bf067c58e64
ogendogen <kosmos500@interia.pl>
2019-12-22 20:35:23 +0100

    PluginsReader created

---
+++ PluginsScanner/PluginsReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace PluginsScanner
+{
+    public static class PluginsReader
+    {
+        public static IEnumerable<Plugin> GetPluginsFromDirectory(string directory, string matchPattern = "*.memory")
+        {
+            string[] files = Directory.GetFiles(directory, matchPattern);
+            foreach (var file in files)
+            {
+                yield return new Plugin
+                { 
+                    Name = Path.GetFileNameWithoutExtension(file),
+                    PluginDump = File.ReadAllLines(file)
+                };
+            }
+        }
+    }
+}
